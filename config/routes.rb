fbbf8556c96872a3f278fc0c403cf4bc97cd219c
Konstantin Samarin <konstsamari@gmail.com>
2021-06-29 18:14:11 +0700

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    resources :diaries, only: %i[index show create update destroy] do
+      resources :notes, only: %i[index create]
+    end
+
+    resources :notes, only: %i[update destroy]
+  end
 end
