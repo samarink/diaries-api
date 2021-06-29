6e045f37920e9edb7a7c3e61eecca2f5990c1530
Konstantin Samarin <konstsamari@gmail.com>
2021-06-29 20:03:16 +0700

    add sidekiq route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
 
   namespace :api, defaults: { format: :json } do
     resources :diaries, only: %i[index show create update destroy] do
