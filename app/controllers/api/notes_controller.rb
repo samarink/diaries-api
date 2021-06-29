1e18c65ca3fc8fd6dbbe53441bed4db410869c64
Konstantin Samarin <konstsamari@gmail.com>
2021-06-29 18:33:15 +0700

    add notes controller

---
+++ app/controllers/api/notes_controller.rb
@@ -0,0 +1,46 @@
+class Api::NotesController < ApplicationController
+  def index
+    diary_id = params[:diary_id]
+
+    render json: Diary.find(diary_id).notes
+  end
+
+  def create
+    diary_id = params[:diary_id]
+    @diary = Diary.find(diary_id)
+    @note = @diary.notes.create(notes_params)
+
+    if @note.save
+      render json: @note, status: :created
+    else
+      render json: @note.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(notes_params)
+      render json: @note, status: :created
+    else
+      render json: @note.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    @note = Note.find(id)
+
+    if @note.destroy
+      render json: @note, status: :no_content
+    else
+      render json: @note.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def notes_params
+    params.require(:note).permit(:text)
+  end
+end
