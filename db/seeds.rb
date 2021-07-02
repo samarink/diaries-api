47bfbf65af673d6b68b8279ddfca32940e34b4ed
Konstantin Samarin <konstsamari@gmail.com>
2021-07-02 17:15:42 +0700

    db/seeds.rb: stylistic improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,34 @@
 Diary.destroy_all
 Note.destroy_all
 
-(1..2).each do |i|
-  diary = Diary.create(title: "public diary N.#{i}", kind: 'public')
+NUM_PUBLIC_DIARIES = 2
+NUM_PRIVATE_DIARIES = 2
+NUM_NOTES_FOR_PUBLIC_DIARIES = 3
+NUM_NOTES_FOR_PRIVATE_DIARIES = 3
 
-  (1..3).each do |j|
-    diary.notes.create(text: "text of the note N.#{j} for diary N.#{i}")
+# Create public diaries
+(1..NUM_PUBLIC_DIARIES).each do |i|
+  diary = Diary.create(
+    title: "public diary N.#{i}",
+    kind: 'public'
+  )
+
+  # Create notes for each diary
+  (1..NUM_NOTES_FOR_PUBLIC_DIARIES).each do |j|
+    diary.notes.create(text: "text of the note N.#{j} for public diary N.#{i}")
   end
 end
 
-(1..2).each do |i|
-  diary = Diary.create(title: "private diary N.#{i}", kind: 'private', expiration: 10.minutes.from_now)
+# Create private diaries
+(1..NUM_PRIVATE_DIARIES).each do |i|
+  diary = Diary.create(
+    title: "private diary N.#{i}",
+    kind: 'private',
+    expiration: 10.minutes.from_now
+  )
 
-  (1..3).each do |j|
-    diary.notes.create(text: "text of the note N.#{j} for diary N.#{i}")
+  # Create notes for each diary
+  (1..NUM_NOTES_FOR_PRIVATE_DIARIES).each do |j|
+    diary.notes.create(text: "text of the note N.#{j} for private diary N.#{i}")
   end
 end
